c5bfaf153f4041f0d640f34412b1f809bd705190
Anu Tomy <anuatomy@gmail.com>
2018-01-08 22:23:38 -0500

    Create Bulldozer class

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-
     public static void main(String[] args) {
+
         Bulldozer bulldozer1 = new Bulldozer("Yellow", "Small");
         Bulldozer bulldozer2 = new Bulldozer("Red", "Large");
 
