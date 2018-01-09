e31a053ffaa5637d4371af7f84d09eb700bfdb59
Anu Tomy <anuatomy@gmail.com>
2018-01-08 22:17:02 -0500

    Initial Commit

---
+++ Classes/src/Bulldozer.java
@@ -0,0 +1,26 @@
+public class Bulldozer {
+
+    private String color;
+    private String size;
+
+    public Bulldozer(String color, String size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
---
+++ Classes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Bulldozer bulldozer1 = new Bulldozer("Yellow", "Small");
+        Bulldozer bulldozer2 = new Bulldozer("Red", "Large");
+
+        System.out.println("Bulldozer 1 is : " + bulldozer1.getColor());
+        System.out.println("Bulldozer 2 is : " + bulldozer2.getColor());
+
+    }
+}
